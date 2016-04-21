f2deb2a2c6a98725fc6c8e7028d439344cb257a9
Marcel Visser <marcel1337@hotmail.com>
2016-04-21 09:01:01 +0200

    Added slack integration to travis

---
+++ imports/test/Server/main.test.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+console.log("Test loaded.");
+function add() {
+  return Array.prototype.slice.call(arguments).reduce(function(prev, curr) {
+    return prev + curr;
+  }, 0);
+}
+
+describe('add()', function() {
+  var tests = [
+    {args: [1, 2],       expected: 3},
+    {args: [1, 2, 3],    expected: 6},
+    {args: [1, 2, 3, 4], expected: 10},
+    {args: [1, 2, 3, 4], expected: 12},
+    {args: [1, 2, 3, 4], expected: 14}
+  ];
+
+  tests.forEach(function(test) {
+    it('correctly adds ' + test.args.length + ' args', function() {
+      var res = add.apply(null, test.args);
+      assert.equal(res, test.expected);
+    });
+  });
+});
