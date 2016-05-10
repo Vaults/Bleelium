cec47cdae605025596c28c5f2fcdb15436d0e672
mannershark <arjanboogaart@gmail.com>
2016-05-10 10:47:51 +0200

    Added CSS and simple navigation bar

--- client/main.js
+++ client/main.js
@@ -1,6 +1,24 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-angular.module('socially', [
+angular.module('dashboard', [
     angularMeteor
-]);
+])
+    .directive('navBar', function(){
+        return {
+            templateUrl: 'client/nav-bar.html',
+            scope: {
+                categories: '=',
+            }
+        };
+    })
+    .controller('dashboardCtrl', function($scope){
+
+    $scope.categories = [
+        {name:'MOBILITY', color: '#ea5959'},
+        {name:'WEATHER', color: '#eb9860'},
+        {name:'SECURITY', color: '#52acdb'},
+        {name:'COMFORT', color: '#70cf7d'},
+        {name:'ENERGY', color: '#f3db36'},
+    ]
+})
