b6710160e316059bf727c329a240ec946bf43f76
mannershark <arjanboogaart@gmail.com>
2016-05-11 09:15:00 +0200

    Active tab now has class=active

--- client/main.js
+++ client/main.js
@@ -1,26 +1,24 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 
-angular.module('dashboard', [
+var dashboard = angular.module('dashboard', [
     angularMeteor
 ])
 .directive('navBar', function(){
         return {
             templateUrl: 'client/nav-bar.html',
+            scope: '=',
         };
     })
-.controller('navBarCtrl', function($scope){
+.controller('navBarCtrl', function($scope, $location){
+    $scope.navClass = function(path) {
+        return (($location.path().substr(1, path.length) === path) ? 'active' : '');
+    };
     $scope.categories = [
-        {link: 'mobility', text:'MOBILITY', color: '#ea5959'},
+        {link: "mobility", text:'MOBILITY', color: '#ea5959'},
         {link: 'weather', text:'WEATHER', color: '#eb9860'},
         {link: 'security', text:'SECURITY', color: '#52acdb'},
         {link: 'comfort', text:'COMFORT', color: '#70cf7d'},
         {link: 'energy', text:'ENERGY', color: '#f3db36'},
-    ]
-    $scope.navClass = function(page) {
-        var currentRoute = $location.path().substring(1) || 'weather';
-        if (page === currentRoute){ return 'active'}
-        return '';
-        //return page === currentRoute ? 'active' : '';
-    };
+    ];
 });
