2be14849945ab52699c9517c88f5efb213ae735f
Marcel Visser <marcel1337@hotmail.com>
2016-05-17 15:40:10 +0200

    Implemented simple UI router functionality

--- client/js/controllers/mainModule.js
+++ client/js/controllers/mainModule.js
@@ -2,14 +2,24 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import 'angular-simple-logger';
 import 'angular-google-maps';
+import 'angular-ui-bootstrap';
 
 var MAIN_MODULE;
 if(!MAIN_MODULE) {
     MAIN_MODULE = angular.module('dashboard', [
         angularMeteor,
         'nemLogging',
-        'uiGmapgoogle-maps'
-    ]);
+        'uiGmapgoogle-maps',
+		'ui.bootstrap',
+		'ui.router'
+    ]).config(function($stateProvider, $urlRouterProvider){
+			$urlRouterProvider.otherwise('/weather');
+			$stateProvider.state('weather',
+				{
+					url:'/weather',
+					templateUrl: 'client/js/directives/infoWeather.html'
+				});
+	});
 }
 
 export {MAIN_MODULE}
