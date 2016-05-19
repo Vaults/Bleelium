c2aad780df54a64e61b8c47d341a31955a5afdb1
Martijn Copier <martijn_copier_4@hotmail.com>
2016-05-19 09:57:04 +0200

    Serverside winddir

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,5 +1,4 @@
 import {MAIN_MODULE} from  './mainModule.js';
-import {WIND_DIR} from './windDirections.js';
 
 MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $rootScope) {
 
@@ -15,18 +14,6 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
         }
     });
 
-	$scope.findWindDir = function(degrees){
-		var min = 360;
-		var answer = '';
-
-		for(var key in WIND_DIR){
-				if ((Math.abs(degrees - (WIND_DIR[key].deg)) < min)) {
-                    min = degrees - WIND_DIR[key].deg;
-					answer = WIND_DIR[key].name;
-				}
-		}
-		return answer;
-	}
 
     $scope.findWeatherStationInfo = function (loc) {
         var selector = {'attributes.coord_lat': String(lodash.round(loc.lat(),2)), 'attributes.coord_lon': String(lodash.round(loc.lng(),2))};
@@ -49,7 +36,7 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
              $scope.latitude = lodash.round(arg.lat(),2);
              $scope.longtitude = lodash.round(arg.lng(),2);
              $scope.temperature = loc.attributes.temp;
-			 $scope.windDirection = $scope.findWindDir(loc.attributes.wind_deg);
+			 $scope.windDirection = getWindDir(loc.attributes.wind_deg);
 			 $scope.Airpressure = lodash.round(loc.attributes.pressure);
 			 $scope.Humidity = lodash.round(loc.attributes.humidity);
              $scope.sunrise = loc.attributes.sunrise;
@@ -58,6 +45,14 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
              $scope.$apply();
          }
      };
+
+	var getWindDir = function (degrees) {
+		Meteor.call('findWindDir', degrees , function(error, result){
+			$scope.windDirection = result;
+		})
+	}
+
+
 
     $scope.$on('setInfo', setInfo);
     $scope.setLocation = function (latitude, longitude) {
@@ -156,3 +151,16 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
 
 
 });
+/*
+Meteor.methods({
+	'testFunc': function(a, b, c){
+	// code;
+	}
+}
+
+Meteor.call('testFunc', a , b, c, function(error, result){
+
+	use(result)
+
+});
+ */
--- server/main.js
+++ server/main.js
@@ -1,21 +1,22 @@
-import { HTTP } from 'meteor/http';
-import { EJSON } from 'meteor/ejson';
-import { Mongo } from 'meteor/mongo';
+import {HTTP} from 'meteor/http';
+import {EJSON} from 'meteor/ejson';
+import {Mongo} from 'meteor/mongo';
+import {WIND_DIR} from './windDirections.js';
 
 WeatherStations = new Mongo.Collection('weatherStations');
-Meteor.publish('weatherPub', function weatherPublication(){
-  return WeatherStations.find({});
+Meteor.publish('weatherPub', function weatherPublication() {
+    return WeatherStations.find({});
 })
 
 var dataIDmap = {
-	"2750953" : "Eindhoven",
-	"2745706" : "Veldhoven",
-	"2754447" : "Helmond",
-	"2759794" : "Amsterdam"
+    "2750953": "Eindhoven",
+    "2745706": "Veldhoven",
+    "2754447": "Helmond",
+    "2759794": "Amsterdam"
 }
-var query = {data:{entities:[]}};
-for(key in dataIDmap){
-	query.data.entities.push({
+var query = {data: {entities: []}};
+for (key in dataIDmap) {
+    query.data.entities.push({
         "type": "WeatherStation",
         "isPattern": "false",
         "id": key
@@ -25,177 +26,191 @@ for(key in dataIDmap){
 
 /* https://github.com/percolatestudio/meteor-synced-cron */
 SyncedCron.add({
-	name: 'Collect the OpenWeatherMap data and store it into orion',
-	schedule: function(parser) {
-		// parser is a later.parse object
-		return parser.text('every 20 seconds');
-	},
-	job: function() {
-		var update = function(locations){
-			HTTP.call( 'GET',  "http://api.openweathermap.org/data/2.5/group?appid=ec57dc1b5b186be9c7900a63a3e34066&id="+locations+ "&units=metric", {}, function( error, response ) {
-				if ( error ) {
-					console.log( error );
-				} else {
-					for (i = 0; i < response.data.cnt; i++) {
-						var weatherdata =  {
-								"contextElements": [
-									{
-										"type": "WeatherStation",
-										"isPattern": "false",
-										"id": response.data.list[i].id,
-										"attributes": [
-											{
-											"name": "name",
-													"type": "string",
-													"value": response.data.list[i].name
-											},
-											{
-											"name": "location",
-													"type": "string",
-													"value": response.data.list[i].name,
-											},
-											{
-											"name": "coord_lon",
-													"type": "float",
-													"value": response.data.list[i].coord.lon
-											},
-											{
-											"name": "coord_lat",
-													"type": "float",
-													"value": response.data.list[i].coord.lat
-											},
-											{
-											"name": "weather_main",
-													"type": "string",
-													"value": response.data.list[i].weather[0].main
-											},
-											{
-											"name": "weather_description",
-													"type": "string",
-													"value": response.data.list[i].weather[0].description
-											},
-											{
-											"name": "weather_icon",
-													"type": "string",
-													"value": response.data.list[i].weather[0].icon
-											},
-											{
-											"name": "temp",
-													"type": "float",
-													"value": response.data.list[i].main.temp
-											},
-											{
-											"name": "pressure",
-													"type": "float",
-													"value": response.data.list[i].main.pressure
-											},
-											{
-											"name": "humidity",
-													"type": "float",
-													"value": response.data.list[i].main.humidity
-											},
-											{
-											"name": "temp_min",
-													"type": "float",
-													"value": response.data.list[i].main.temp_min
-											},
-											{
-											"name": "temp_max",
-													"type": "float",
-													"value": response.data.list[i].main.temp_max
-											},
-											{
-											"name": "wind_speed",
-													"type": "float",
-													"value": response.data.list[i].wind.speed
-											},
-											{
-											"name": "wind_deg",
-													"type": "float",
-													"value": response.data.list[i].wind.deg
-											},
-											{
-											"name": "country",
-													"type": "string",
-													"value": response.data.list[i].sys.country
-											},
-											{
-											"name": "sunrise",
-													"type": "int",
-													"value": response.data.list[i].sys.sunrise
-											},
-											{
-											"name": "sunset",
-													"type": "int",
-													"value": response.data.list[i].sys.sunset
-											}
-										]
-									}
-								],
-								"updateAction": "APPEND"
-						};
+    name: 'Collect the OpenWeatherMap data and store it into orion',
+    schedule: function (parser) {
+        // parser is a later.parse object
+        return parser.text('every 20 seconds');
+    },
+    job: function () {
+        var update = function (locations) {
+            HTTP.call('GET', "http://api.openweathermap.org/data/2.5/group?appid=ec57dc1b5b186be9c7900a63a3e34066&id=" + locations + "&units=metric", {}, function (error, response) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    for (i = 0; i < response.data.cnt; i++) {
+                        var weatherdata = {
+                            "contextElements": [
+                                {
+                                    "type": "WeatherStation",
+                                    "isPattern": "false",
+                                    "id": response.data.list[i].id,
+                                    "attributes": [
+                                        {
+                                            "name": "name",
+                                            "type": "string",
+                                            "value": response.data.list[i].name
+                                        },
+                                        {
+                                            "name": "location",
+                                            "type": "string",
+                                            "value": response.data.list[i].name,
+                                        },
+                                        {
+                                            "name": "coord_lon",
+                                            "type": "float",
+                                            "value": response.data.list[i].coord.lon
+                                        },
+                                        {
+                                            "name": "coord_lat",
+                                            "type": "float",
+                                            "value": response.data.list[i].coord.lat
+                                        },
+                                        {
+                                            "name": "weather_main",
+                                            "type": "string",
+                                            "value": response.data.list[i].weather[0].main
+                                        },
+                                        {
+                                            "name": "weather_description",
+                                            "type": "string",
+                                            "value": response.data.list[i].weather[0].description
+                                        },
+                                        {
+                                            "name": "weather_icon",
+                                            "type": "string",
+                                            "value": response.data.list[i].weather[0].icon
+                                        },
+                                        {
+                                            "name": "temp",
+                                            "type": "float",
+                                            "value": response.data.list[i].main.temp
+                                        },
+                                        {
+                                            "name": "pressure",
+                                            "type": "float",
+                                            "value": response.data.list[i].main.pressure
+                                        },
+                                        {
+                                            "name": "humidity",
+                                            "type": "float",
+                                            "value": response.data.list[i].main.humidity
+                                        },
+                                        {
+                                            "name": "temp_min",
+                                            "type": "float",
+                                            "value": response.data.list[i].main.temp_min
+                                        },
+                                        {
+                                            "name": "temp_max",
+                                            "type": "float",
+                                            "value": response.data.list[i].main.temp_max
+                                        },
+                                        {
+                                            "name": "wind_speed",
+                                            "type": "float",
+                                            "value": response.data.list[i].wind.speed
+                                        },
+                                        {
+                                            "name": "wind_deg",
+                                            "type": "float",
+                                            "value": response.data.list[i].wind.deg
+                                        },
+                                        {
+                                            "name": "country",
+                                            "type": "string",
+                                            "value": response.data.list[i].sys.country
+                                        },
+                                        {
+                                            "name": "sunrise",
+                                            "type": "int",
+                                            "value": response.data.list[i].sys.sunrise
+                                        },
+                                        {
+                                            "name": "sunset",
+                                            "type": "int",
+                                            "value": response.data.list[i].sys.sunset
+                                        }
+                                    ]
+                                }
+                            ],
+                            "updateAction": "APPEND"
+                        };
 
-						/* proof that OpenWeatherMap is messing up
-						console.log(response.data);
-						console.log('weatherdata:', loc, id, weatherdata.contextElements[0].attributes[0], weatherdata.contextElements[0].attributes[7], response.data.main.temp, response.data.name);
-						*/
+                        /* proof that OpenWeatherMap is messing up
+                         console.log(response.data);
+                         console.log('weatherdata:', loc, id, weatherdata.contextElements[0].attributes[0], weatherdata.contextElements[0].attributes[7], response.data.main.temp, response.data.name);
+                         */
 
-						HTTP.call( 'POST', 'http://131.155.70.152:1026/v1/updateContext', {data: weatherdata},	function( error2, response2 ) {
-							if ( error2 ) {
-								console.log( error2 );
-							}
-						});
-					}
-				}
-			});
-		}
+                        HTTP.call('POST', 'http://131.155.70.152:1026/v1/updateContext', {data: weatherdata}, function (error2, response2) {
+                            if (error2) {
+                                console.log(error2);
+                            }
+                        });
+                    }
+                }
+            });
+        }
 
-    for(key in dataIDmap){
-    	if(!locations){
-				var locations = key;
-			}
-      else {
-         locations = locations+','+key
-      }
+        for (key in dataIDmap) {
+            if (!locations) {
+                var locations = key;
+            }
+            else {
+                locations = locations + ',' + key
+            }
+        }
+        update(locations);
     }
-		update(locations);
-  }
 });
 
 
+var pull = function () {
 
-var pull = function(){
+    HTTP.call('POST', 'http://131.155.70.152:1026/v1/queryContext', query, function (error, response) {
 
-	HTTP.call( 'POST', 'http://131.155.70.152:1026/v1/queryContext', query, function( error, response ) {
-
-		if ( error ) {
-			console.log( error );
-		} else {
-			WeatherStations.remove({});
-			var attributesToKeyValue= function(attr){
-				var temp = {}
-				attr.forEach(function(o){
-					temp[o.name] = o.value;
-				});
-				return temp;
-			}
-			var rewriteAndInsertAttributes = function(obj){
-				for(var i = 0; i < obj.data.contextResponses.length; i++){
-						var tempobj = obj.data.contextResponses[i].contextElement;
-						tempobj.attributes = attributesToKeyValue(tempobj.attributes);
-						WeatherStations.insert(tempobj);
-				}
-			}
-			rewriteAndInsertAttributes(response);
-		}
-	});
+        if (error) {
+            console.log(error);
+        } else {
+            WeatherStations.remove({});
+            var attributesToKeyValue = function (attr) {
+                var temp = {}
+                attr.forEach(function (o) {
+                    temp[o.name] = o.value;
+                });
+                return temp;
+            }
+            var rewriteAndInsertAttributes = function (obj) {
+                for (var i = 0; i < obj.data.contextResponses.length; i++) {
+                    var tempobj = obj.data.contextResponses[i].contextElement;
+                    tempobj.attributes = attributesToKeyValue(tempobj.attributes);
+                    WeatherStations.insert(tempobj);
+                }
+            }
+            rewriteAndInsertAttributes(response);
+        }
+    });
 };
-var reloadPull = function(){
-	pull();
-	Meteor.setTimeout(reloadPull, 5000);
+var reloadPull = function () {
+    pull();
+    Meteor.setTimeout(reloadPull, 5000);
 }
 
-if(!Meteor.isTest){
-	SyncedCron.start();
-	reloadPull();
+Meteor.methods({
+    'findWindDir': function (degrees) {
+            var min = 360;
+            var answer = '';
+
+            for(var key in WIND_DIR){
+                if ((Math.abs(degrees - (WIND_DIR[key].deg)) < min)) {
+                    min = degrees - WIND_DIR[key].deg;
+                    answer = WIND_DIR[key].name;
+                }
+            }
+            return answer;
+        }
+})
+
+if (!Meteor.isTest) {
+    SyncedCron.start();
+    reloadPull();
 }
