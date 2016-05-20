8a6c8724f1062d7924f8fcbfdb1c1c3e0e9eb4b6
maartendemandt <maartendemandt@gmail.com>
2016-05-20 11:42:49 +0200

    implementing P2000

--- server/main.js
+++ server/main.js
@@ -120,7 +120,7 @@ var pushWeatherToOrion = function () { //Sends all data pulled from OpenWeatherM
                 console.log(error);
             } else {
                 for (i = 0; i < response.data.cnt; i++) {
-					postOrionData(createWeatherData(response.data.list[i]));  
+					postOrionData(createWeatherData(response.data.list[i]));
                 }
             }
         });
@@ -137,6 +137,56 @@ var pushWeatherToOrion = function () { //Sends all data pulled from OpenWeatherM
     update(locations);
 }
 
+var createP2000Data = function(o){ //Creates orion-compliant objects for Orion storage
+	return {
+		"contextElements": [
+			{
+				"type": "P2000",
+				"isPattern": "false",
+				"id": o.guid[0]._,
+				"attributes": [
+          {
+            "name": "title",
+            "type": "string",
+            "value": o.title[0]
+          },
+          {
+            "name": "description",
+            "type": "string",
+            "value": o.description[0].replace(/\<(.*?)\>/g, '').replace('(', '').replace(')', '')
+          },
+					{
+						"name": "publish_date",
+						"type": "string",
+						"value": o.pubDate[0]
+					}
+				]
+			}
+		],
+		"updateAction": "APPEND"
+	};
+}
+var pushP2000ToOrion = function() {
+  HTTP.call('GET', 'http://feeds.livep2000.nl/?r=22&d=1,2,3', function(error, response) {
+    if (error) {
+        console.log(error);
+    } else {
+      xml2js.parseString(response.content, function (err, result) {
+        for(item in result.rss.channel[0].item) {
+          data = createP2000Data(result.rss.channel[0].item[item]);
+          HTTP.call('POST', 'http://131.155.70.152:1026/v1/updateContext', {data: data}, function(error, response) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(response);
+              }
+          });
+        }
+      });
+    }
+  });
+}
+
 /* https://github.com/percolatestudio/meteor-synced-cron */
 SyncedCron.add({	//calls pushWeatherToOrion every 30 mins
     name: 'Pushing weather to Orion',
@@ -144,6 +194,14 @@ SyncedCron.add({	//calls pushWeatherToOrion every 30 mins
         return parser.text('every 30 minutes');
     },
     job: pushWeatherToOrion
+});
+
+SyncedCron.add({	//calls pushWeatherToOrion every 30 mins
+    name: 'Pushing P2000 to Orion',
+    schedule: function (parser) {
+        return parser.text('every 20 seconds');
+    },
+    job: pushP2000ToOrion
 });
 
 if (!Meteor.isTest) { //only polls data getting/setting if the system is not in test mode
