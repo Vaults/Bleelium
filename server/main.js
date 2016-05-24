c4dcb24a05c8ef09773a30d35300c3cefd5f5438
Martijn Copier <martijn_copier_4@hotmail.com>
2016-05-24 13:58:35 +0200

    niks

--- server/main.js
+++ server/main.js
@@ -7,163 +7,163 @@ import {collectionWrapper} from '/server/imports/collections.js';
 import {rewriteAttributes} from '/server/imports/util.js';
 
 var dataWeatherMap = {
-	"2750953": "Mensfort",
-	"2756253": "Eindhoven",
-	"2745706": "Veldhoven",
-	"2754447": "Helmond",
-	"2759794": "Amsterdam"
+    "2750953": "Mensfort",
+    "2756253": "Eindhoven",
+    "2745706": "Veldhoven",
+    "2754447": "Helmond",
+    "2759794": "Amsterdam"
 }
 var weatherQuery = initQuery("WeatherStation", dataWeatherMap);
 
-var createWeatherData = function(o){ //Creates orion-compliant objects for Orion storage
-	return {
-		"contextElements": [
-			{
-				"type": "WeatherStation",
-				"isPattern": "false",
-				"id": o.id,
-				"attributes": [
-					{
-						"name": "name",
-						"type": "string",
-						"value": o.name
-					},
-					{
-						"name": "location",
-						"type": "string",
-						"value": o.name,
-					},
-					{
-						"name": "coord_lon",
-						"type": "float",
-						"value": o.coord.lon
-					},
-					{
-						"name": "coord_lat",
-						"type": "float",
-						"value": o.coord.lat
-					},
-					{
-						"name": "weather_main",
-						"type": "string",
-						"value": o.weather[0].main
-					},
-					{
-						"name": "weather_description",
-						"type": "string",
-						"value": o.weather[0].description
-					},
-					{
-						"name": "weather_icon",
-						"type": "string",
-						"value": o.weather[0].icon
-					},
-					{
-						"name": "temp",
-						"type": "float",
-						"value": o.main.temp
-					},
-					{
-						"name": "pressure",
-						"type": "float",
-						"value": o.main.pressure
-					},
-					{
-						"name": "humidity",
-						"type": "float",
-						"value": o.main.humidity
-					},
-					{
-						"name": "temp_min",
-						"type": "float",
-						"value": o.main.temp_min
-					},
-					{
-						"name": "temp_max",
-						"type": "float",
-						"value": o.main.temp_max
-					},
-					{
-						"name": "wind_speed",
-						"type": "float",
-						"value": o.wind.speed
-					},
-					{
-						"name": "wind_deg",
-						"type": "float",
-						"value": o.wind.deg
-					},
-					{
-						"name": "country",
-						"type": "string",
-						"value": o.sys.country
-					},
-					{
-						"name": "sunrise",
-						"type": "int",
-						"value": o.sys.sunrise
-					},
-					{
-						"name": "sunset",
-						"type": "int",
-						"value": o.sys.sunset
-					}
-				]
-			}
-		],
-		"updateAction": "APPEND"
-	};
+var createWeatherData = function (o) { //Creates orion-compliant objects for Orion storage
+    return {
+        "contextElements": [
+            {
+                "type": "WeatherStation",
+                "isPattern": "false",
+                "id": o.id,
+                "attributes": [
+                    {
+                        "name": "name",
+                        "type": "string",
+                        "value": o.name
+                    },
+                    {
+                        "name": "location",
+                        "type": "string",
+                        "value": o.name,
+                    },
+                    {
+                        "name": "coord_lon",
+                        "type": "float",
+                        "value": o.coord.lon
+                    },
+                    {
+                        "name": "coord_lat",
+                        "type": "float",
+                        "value": o.coord.lat
+                    },
+                    {
+                        "name": "weather_main",
+                        "type": "string",
+                        "value": o.weather[0].main
+                    },
+                    {
+                        "name": "weather_description",
+                        "type": "string",
+                        "value": o.weather[0].description
+                    },
+                    {
+                        "name": "weather_icon",
+                        "type": "string",
+                        "value": o.weather[0].icon
+                    },
+                    {
+                        "name": "temp",
+                        "type": "float",
+                        "value": o.main.temp
+                    },
+                    {
+                        "name": "pressure",
+                        "type": "float",
+                        "value": o.main.pressure
+                    },
+                    {
+                        "name": "humidity",
+                        "type": "float",
+                        "value": o.main.humidity
+                    },
+                    {
+                        "name": "temp_min",
+                        "type": "float",
+                        "value": o.main.temp_min
+                    },
+                    {
+                        "name": "temp_max",
+                        "type": "float",
+                        "value": o.main.temp_max
+                    },
+                    {
+                        "name": "wind_speed",
+                        "type": "float",
+                        "value": o.wind.speed
+                    },
+                    {
+                        "name": "wind_deg",
+                        "type": "float",
+                        "value": o.wind.deg
+                    },
+                    {
+                        "name": "country",
+                        "type": "string",
+                        "value": o.sys.country
+                    },
+                    {
+                        "name": "sunrise",
+                        "type": "int",
+                        "value": o.sys.sunrise
+                    },
+                    {
+                        "name": "sunset",
+                        "type": "int",
+                        "value": o.sys.sunset
+                    }
+                ]
+            }
+        ],
+        "updateAction": "APPEND"
+    };
 }
 
-var createForecastData = function(o, i, id){
-	return {
-		"contextElements": [
-			{
-				"type": "WeatherStation",
-				"isPattern": "false",
-				"id": id,
-				"attributes": [
-					{
-						"name": i + '-' + 'timestamp',
-						"type": "string",
-						"value": o.dt
-					},
-					{
-						"name": i + '-' + 'pressure',
-						"type": "string",
-						"value": o.pressure
-					},
-					{
-						"name": i + '-' + 'humidity',
-						"type": "string",
-						"value": o.humidity
-					},
-					{
-						"name": i + '-' + 'icon',
-						"type": "string",
-						"value": o.weather[0]['icon']
-					},
-					{
-						"name": i + '-' + 'deg',
-						"type": "string",
-						"value": o.deg
-					},
-					{
-						"name": i + '-' + 'min',
-						"type": "string",
-						"value": o.temp.min
-					},
-					{
-						"name": i + '-' + 'max',
-						"type": "string",
-						"value": o.temp.max
-					},
+var createForecastData = function (o, i, id) {
+    return {
+        "contextElements": [
+            {
+                "type": "WeatherStation",
+                "isPattern": "false",
+                "id": id,
+                "attributes": [
+                    {
+                        "name": i + '-' + 'timestamp',
+                        "type": "string",
+                        "value": o.dt
+                    },
+                    {
+                        "name": i + '-' + 'pressure',
+                        "type": "string",
+                        "value": o.pressure
+                    },
+                    {
+                        "name": i + '-' + 'humidity',
+                        "type": "string",
+                        "value": o.humidity
+                    },
+                    {
+                        "name": i + '-' + 'icon',
+                        "type": "string",
+                        "value": o.weather[0]['icon']
+                    },
+                    {
+                        "name": i + '-' + 'deg',
+                        "type": "string",
+                        "value": o.deg
+                    },
+                    {
+                        "name": i + '-' + 'min',
+                        "type": "string",
+                        "value": o.temp.min
+                    },
+                    {
+                        "name": i + '-' + 'max',
+                        "type": "string",
+                        "value": o.temp.max
+                    },
 
-				]
-			}
-		],
-		"updateAction": "APPEND"
-	};
+                ]
+            }
+        ],
+        "updateAction": "APPEND"
+    };
 }
 
 var pushWeatherToOrion = function () { //Sends all data pulled from OpenWeatherMap to Orion
@@ -173,99 +173,117 @@ var pushWeatherToOrion = function () { //Sends all data pulled from OpenWeatherM
                 console.log(error);
             } else {
                 for (i = 0; i < response.data.cnt; i++) {
-					postOrionData(createWeatherData(response.data.list[i]));
+                    postOrionData(createWeatherData(response.data.list[i]));
                 }
-				for(var j = 0; j < locs.length; j++){
-					var loc = locs[j];
-					while(locs.length > 0){
-						var loc = locs.pop();
-						HTTP.call('GET', "http://api.openweathermap.org/data/2.5/forecast/daily?appid=ec57dc1b5b186be9c7900a63a3e34066&id=" + loc + "&units=metric", {}, function (error, response) {
-							if (error) {
-								console.log(error);
-							} else {
-								for (i = 1; i < response.data.list.length; i++) {
-									postOrionData(createForecastData(response.data.list[i], i, response.data.city.id));
-								}
-							}
-						});
-					}
-				}
+                for (var j = 0; j < locs.length; j++) {
+                    var loc = locs[j];
+                    while (locs.length > 0) {
+                        var loc = locs.pop();
+                        HTTP.call('GET', "http://api.openweathermap.org/data/2.5/forecast/daily?appid=ec57dc1b5b186be9c7900a63a3e34066&id=" + loc + "&units=metric", {}, function (error, response) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                for (i = 1; i < response.data.list.length; i++) {
+                                    postOrionData(createForecastData(response.data.list[i], i, response.data.city.id));
+                                }
+                            }
+                        });
+                    }
+                }
             }
         });
-	}
-	var locs = [];
-	var locations = '';
-	for (key in dataWeatherMap) {
-		locs.push(key);
-		if (!locations) {
-			var locationString = key;
-		}
-		else {
-			locationString = locationString + ',' + key
-		}
-	}
+    }
+    var locs = [];
+    var locations = '';
+    for (key in dataWeatherMap) {
+        locs.push(key);
+        if (!locations) {
+            var locationString = key;
+        }
+        else {
+            locationString = locationString + ',' + key
+        }
+    }
     update(locationString, locs);
 }
 
-var createP2000Data = function(o){ //Creates orion-compliant objects for Orion storage
-	return {
-		"contextElements": [
-			{
-				"type": "P2000",
-				"isPattern": "false",
-				"id": o.guid[0]._,
-				"attributes": [
-          {
-            "name": "title",
-            "type": "string",
-            "value": o.title[0]
-          },
-          {
-            "name": "description",
-            "type": "string",
-            "value": o.description[0].replace(/\<(.*?)\>/g, '').replace('(', '').replace(')', '')
-          },
-					{
-						"name": "publish_date",
-						"type": "string",
-						"value": o.pubDate[0]
-					}
-				]
-			}
-		],
-		"updateAction": "APPEND"
-	};
-}
-var pushP2000ToOrion = function() {
-  HTTP.call('GET', 'http://feeds.livep2000.nl/?r=22&d=1,2,3', function(error, response) {
-    if (error) {
-        console.log(error);
-    } else {
-      xml2js.parseString(response.content, function (err, result) {
-        for(item in result.rss.channel[0].item) {
-          data = createP2000Data(result.rss.channel[0].item[item]);
-          HTTP.call('POST', 'http://131.155.70.152:1026/v1/updateContext', {data: data}, function(error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-              }
-          });
-        }
-        var collection = collectionWrapper['P2000'];
-        HTTP.call('GET', 'http://131.155.70.152:1026/v1/contextEntityTypes/P2000', function (error, response) {
-            if (error) {
-                console.log(error);
-            } else {
-              response = rewriteAttributes(response);
-              for(item in response.data.contextResponses) {
-                collectionWrapper['P2000'].insert(response.data.contextResponses[item].contextElement);
-              }
+var createP2000Data = function (o) { //Creates orion-compliant objects for Orion storage
+    return {
+        "contextElements": [
+            {
+                "type": "P2000",
+                "isPattern": "false",
+                "id": o.guid[0]._,
+                "attributes": [
+                    {
+                        "name": "title",
+                        "type": "string",
+                        "value": o.title[0]
+                    },
+                    {
+                        "name": "description",
+                        "type": "string",
+                        "value": o.description[0].replace(/\<(.*?)\>/g, '').replace('(', '').replace(')', '')
+                    },
+                    {
+                        "name": "publish_date",
+                        "type": "string",
+                        "value": o.pubDate[0]
+                    },
+                    {
+                        "name": "coord_lat",
+                        "type": "string",
+                        "value": o['geo:lat']
+                    },
+                    {
+                        "name": "coord_lng",
+                        "type": "string",
+                        "value": o['geo:long']
+                    }
+                ]
             }
-        });
+        ],
+        "updateAction": "APPEND"
+    };
+}
 
-      });
-    }
-  });
+var pushP2000ToOrion = function () {
+    HTTP.call('GET', 'http://feeds.livep2000.nl/?r=22&d=1,2,3', function (error, response) {
+        if (error) {
+            console.log(error);
+        } else {
+            xml2js.parseString(response.content, function (err, result) {
+                for (item in result.rss.channel[0].item) {
+                    data = createP2000Data(result.rss.channel[0].item[item]);
+                    HTTP.call('POST', 'http://131.155.70.152:1026/v1/updateContext', {data: data}, function (error, response) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+
+                        }
+                    });
+                }
+                var collection = collectionWrapper['P2000'];
+                HTTP.call('GET', 'http://131.155.70.152:1026/v1/contextEntityTypes/P2000?orderBy=!publish_date&limit=1000', function (error, response) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response = rewriteAttributes(response);
+                        for (item in response.data.contextResponses) {
+                            if(collectionWrapper['P2000'].findOne(response.data.contextResponses[item].contextElement)){
+
+                                   // !collectionWrapper['P2000'].findOne(response.data.contextResponses[item].contextElement)){
+                            }else {
+                                console.log(response.data.contextResponses[item].contextElement);
+                                collectionWrapper['P2000'].insert(response.data.contextResponses[item].contextElement);
+                            }
+                        }
+                    }
+                });
+
+            });
+        }
+    });
 }
 
 /* https://github.com/percolatestudio/meteor-synced-cron */
@@ -285,46 +303,46 @@ SyncedCron.add({	//calls pushWeatherToOrion every 30 mins
     job: pushP2000ToOrion
 });
 
-var numToObj = function(o){
-	o.forecast = {};
-	for(key in o){
-		var fc = key.charAt(0);
-        if(fc >= 0 && fc <= 9){
-			if(!o.forecast['day' + fc]){
-				o.forecast['day' + fc] = {}
-			}
-			o.forecast['day' + fc][key.substr(2,key.length)] = o[key];
- 			delete o[key];
-		}
+var numToObj = function (o) {
+    o.forecast = {};
+    for (key in o) {
+        var fc = key.charAt(0);
+        if (fc >= 0 && fc <= 9) {
+            if (!o.forecast['day' + fc]) {
+                o.forecast['day' + fc] = {}
+            }
+            o.forecast['day' + fc][key.substr(2, key.length)] = o[key];
+            delete o[key];
+        }
     }
     return o;
 }
 
-var rewriteNumbersToObjects = function(obj){
-	for (var i = 0; i < obj.data.contextResponses.length; i++) {
-		var tempobj = obj.data.contextResponses[i].contextElement;
-		tempobj.attributes = numToObj(tempobj.attributes);
-	}
-	return obj;
+var rewriteNumbersToObjects = function (obj) {
+    for (var i = 0; i < obj.data.contextResponses.length; i++) {
+        var tempobj = obj.data.contextResponses[i].contextElement;
+        tempobj.attributes = numToObj(tempobj.attributes);
+    }
+    return obj;
 }
 
 
 if (!Meteor.isTest) { //only polls data getting/setting if the system is not in test mode
     SyncedCron.start();
 
-	//reloadPull("WeatherStations", weatherQuery, function(args){
-	//	collectionWrapper['WeatherStations'].remove({});
-		//console.log(args.data.contextResponses);
-	//});
-	reloadPull("WeatherStations", weatherQuery, function(args){
-		collectionWrapper['WeatherStations'].remove({});
-		var temp = rewriteAttributes(args);
-		//console.log(args.data);
-		//console.log(rewriteNumbersToObjects(temp).data.contextResponses[0].contextElement.attributes.forecast.day1);
-		rewriteNumbersToObjects(temp).data.contextResponses.forEach(function(o){
-			collectionWrapper['WeatherStations'].insert(o.contextElement);
-		});
-	});
+    //reloadPull("WeatherStations", weatherQuery, function(args){
+    //	collectionWrapper['WeatherStations'].remove({});
+    //console.log(args.data.contextResponses);
+    //});
+    reloadPull("WeatherStations", weatherQuery, function (args) {
+        collectionWrapper['WeatherStations'].remove({});
+        var temp = rewriteAttributes(args);
+        //console.log(args.data);
+        //console.log(rewriteNumbersToObjects(temp).data.contextResponses[0].contextElement.attributes.forecast.day1);
+        rewriteNumbersToObjects(temp).data.contextResponses.forEach(function (o) {
+            collectionWrapper['WeatherStations'].insert(o.contextElement);
+        });
+    });
 }
 
 
