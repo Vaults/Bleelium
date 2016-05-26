6359f45d2bffc1ec201ebd3fc317f359d194ff51
Marcel Visser <marcel1337@hotmail.com>
2016-05-26 19:57:04 +0200

    Resolved all current P2000 front-end tickets

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -157,7 +157,9 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
             }
         }
     }
+
     $scope.autorun(reload)
+
 
 
 }).controller('parkingCtrl', function ($scope, $meteor, $reactive, $rootScope) {
@@ -179,6 +181,32 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
         }
     };
 }).controller('securityCtrl', function ($scope, $meteor, $reactive, $rootScope, $state) {
+    $reactive(this).attach($scope);
+    $scope.eventTypes = {
+        'policedept': {icon: 'img/security/policedept.png', text: 'Police Department', checked: false},
+        'firedept': {icon: 'img/security/firedept.png', text: 'Fire Department', checked: false},
+        'paramedics': {
+            icon: 'img/security/paramedics.png',
+            text: 'Paramedics',
+            checked: true,
+            style: 'margin-bottom: 20px'
+        },
+        'gunshot': {icon: 'img/security/gunshot.png', text: 'Gunshot', checked: false},
+        'stressedvoice': {icon: 'img/security/stressedvoice.png', text: 'Stressed Voice', checked: false},
+        'caralarm': {icon: 'img/security/caralarm.png', text: 'Car Alarm', checked: false},
+        'brokenglass': {icon: 'img/security/brokenglass.png', text: 'Broken Glass', checked: false},
+        'caraccident': {
+            icon: 'img/security/caraccident.png',
+            text: 'Car accident',
+            checked: false,
+            style: 'margin-bottom: 20px'
+        },
+        'warninggeneral': {icon: 'img/security/warninggeneral.png', text: 'Critical Event', checked: false},
+        'warningevacuation': {icon: 'img/security/warningfire2.png', text: 'Evacuation Notice', checked: false},
+        'warningfire': {icon: 'img/security/warningfire.png', text: 'Fire Alarm', checked: false},
+        'warningbombthreat': {icon: 'img/security/warningbombthreat.png', text: 'Bomb Threat', checked: false},
+        'warninggasleak': {icon: 'img/security/warninggasleak.png', text: 'Gas Leak', checked: false}
+    };
 
     $scope.map = {
         center: {
@@ -196,9 +224,10 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
     
     $scope.findEventInfo = function (loc) { //Finds an event from coordinates
         var selector = {
-            'attributes.coord_lat': String(lodash.round(loc.lat(), 2)),
-            'attributes.coord_lng': String(lodash.round(loc.lng(), 2))
+            'attributes.coord_lat': String(loc.lat()),
+            //'attributes.coord_lng': loc.lng()
         };
+
         return P2000.findOne(selector);
     }
     
@@ -215,16 +244,17 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
     var setInfo = function (event, arg) { //Updates scope to the current selected p2000 event
         if (arg) {
             var loc = $scope.findEventInfo(arg);
-            console.log('Initial setinfo loc: ' + loc);
+            console.log(loc);
             $state.go('security.subemergency');
-            //TODO: fill in parameters
+            $scope.city = "Eindhoven";
+            $scope.title = loc.attributes.description;
+            $scope.publish_date = loc.attributes.publish_date;
             $scope.$apply();
         }
     };
     $scope.$on('setInfo', setInfo);
 
     var reload = function () { //Runs whenever the p2000 collection is updated. Pulls all p2000 events and updates all UI elements
-        $reactive(this).attach($scope);
         var events = $scope.getReactively('p2000Events');
         var selEvent = $scope.getReactively('p2000Debug');
         setInfo(null, $scope.loc);
@@ -249,64 +279,56 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
                 };
             }
         }
-        $scope.markers = [];
-        for (var i = 0; i < events.length; i++) {
-            if (events[i].attributes) {
-                //console.log($scope.markers);
-                $scope.markers.push({
-                    options: {
-                        draggable: false,
-                        icon: {
-                            url: '/img/security/paramedics.png',
-                            size: {
-                                height: 600,
-                                width: 600
+
+        console.log($scope.eventTypes.paramedics.checked);
+        if($scope.eventTypes.paramedics.checked) {
+            $scope.markers = [];
+            for (var i = 0; i < events.length; i++) {
+                if (events[i].attributes) {
+                    //console.log($scope.markers);
+                    $scope.markers.push({
+                        options: {
+                            draggable: false,
+                            icon: {
+                                url: '/img/security/paramedics.png',
+                                size: {
+                                    height: 600,
+                                    width: 600
+                                },
+                                anchor: {
+                                    x: 24,
+                                    y: 24
+                                },
+                                scaledSize: {
+                                    height: 48,
+                                    width: 48
+                                }
                             },
-                            anchor: {
-                                x: 24,
-                                y: 24
+                        },
+                        events: {
+                            click: (marker, eventName, args) => {
+                                $rootScope.$broadcast('setInfo', marker.getPosition());
                             },
-                            scaledSize: {
-                                height: 48,
-                                width: 48
+                            dragend: (marker, eventName, args) => {
+                                this.setLocation(marker.getPosition().lat(), marker.getPosition().lng());
+                                $scope.$apply();
                             }
                         },
-                    },
-                    events: {
-                        click: (marker, eventName, args) => {
-                            $rootScope.$broadcast('setInfo', marker.getPosition());
+                        location: {
+                            latitude: events[i].attributes.coord_lat,
+                            longitude: events[i].attributes.coord_lng
                         },
-                        dragend: (marker, eventName, args) => {
-                            this.setLocation(marker.getPosition().lat(), marker.getPosition().lng());
-                            $scope.$apply();
-                        }
-                    },
-                    location: {
-                        latitude: events[i].attributes.coord_lat,
-                        longitude: events[i].attributes.coord_lng
-                    },
-                });
+                    });
+                } else {
+                    $scope.markers[i].setMap(null);
+                }
             }
+        }else{
+            $scope.markers = [];
         }
     }
-    
-    $scope.eventTypes = [
-        {id: 'policedept',          icon: 'img/security/policedept.png',            text: 'Police Department',  checked: 'checked'},
-        {id: 'firedept',            icon: 'img/security/firedept.png',              text: 'Fire Department',    checked: 'checked'},
-        {id: 'paramedics',          icon: 'img/security/paramedics.png',            text: 'Paramedics',         checked: 'checked',     style: 'margin-bottom: 20px'},
-        {id: 'gunshot',             icon: 'img/security/gunshot.png',               text: 'Gunshot',            checked: 'checked'},
-        {id: 'stressedvoice',       icon: 'img/security/stressedvoice.png',         text: 'Stressed Voice',     checked: 'checked'},
-        {id: 'caralarm',            icon: 'img/security/caralarm.png',              text: 'Car Alarm',          checked: 'checked'},
-        {id: 'brokenglass',         icon: 'img/security/brokenglass.png',           text: 'Broken Glass',       checked: 'checked'},
-        {id: 'caraccident',         icon: 'img/security/caraccident.png',           text: 'Car accident',       checked: 'checked',     style: 'margin-bottom: 20px'},
-        {id: 'warninggeneral',      icon: 'img/security/warninggeneral.png',        text: 'Critical Event',     checked: 'checked'},
-        {id: 'warningevacuation',   icon: 'img/security/warningfire2.png',          text: 'Evacuation Notice',  checked: 'checked'},
-        {id: 'warningfire',         icon: 'img/security/warningfire.png',           text: 'Fire Alarm',         checked: 'checked'},
-        {id: 'warningbombthreat',   icon: 'img/security/warningbombthreat.png',     text: 'Bomb Threat',        checked: 'checked'},
-        {id: 'warninggasleak',      icon: 'img/security/warninggasleak.png',        text: 'Gas Leak',           checked: 'checked'}
-    ];
-    
     $scope.autorun(reload);
+    $scope.$watch('eventTypes.paramedics.checked', reload);
     
 }).controller('forecastCtrl', function ($scope, $meteor, $reactive, $rootScope, WeatherService, IconService) {
 
@@ -324,7 +346,7 @@ MAIN_MODULE.controller('weatherCtrl', function($scope, $meteor, $reactive, $root
     $meteor.subscribe('weatherPub');
     //If no location selected, use Eindhoven
     if(WeatherService.weatherLocation == null){
-        WeatherService.weatherLocation = {'attributes.coord_lat': '51.44', 'attributes.coord_lon': '5.48'};
+        WeatherService.weatherLocation = {'attributes.coord_lat': 'false', 'attributes.coord_lon': '5.48'};
     }
 
     //Load the name and coordinates for this location
