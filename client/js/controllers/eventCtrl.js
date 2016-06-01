f5f17132b8ed0e74dbcebf41c30c89952aa874e2
Martijn Copier <Martijn_copier_4@hotmail.com>
2016-06-01 23:51:22 +0200

    Event Trigger is working

--- client/js/controllers/eventCtrl.js
+++ client/js/controllers/eventCtrl.js
@@ -17,58 +17,9 @@ MAIN_MODULE.controller('eventCtrl', function($scope, $meteor, $reactive, $rootSc
 
     var popUpMulti = document.getElementById('pop-upMulti');
 
-    $scope.events = [
-        {
-            coord: {
-                lot: 5.487589,
-                lat: 51.447835
-            },
-            street: "Woenselse Watermolen",
-            description: {
-                name: "Bomb Threat",
-                sensor: "bomb detector",
-                level: 9
-            },
-            time: 1530,
-            date: 24052016,
-            view: true,
-            icon: "warningbombthreat"
-        },
-        {
-            coord: {
-                lot: 5.487589,
-                lat: 51.447835
-            },
-            street: "Gildebuurt",
-            description: {
-                name: "Gas Leak",
-                sensor: "gas detector",
-                level: 10
-            },
-            time: 1630,
-            date: 24052016,
-            view: true,
-            icon: "warninggasleak"
-        },
-        {
-            coord: {
-                lot: 5.487589,
-                lat: 51.447835
-            },
-            street: "Witte Dame",
-            description: {
-                name: "Car Accident",
-                sensor: "sound sensor",
-                level: 7
-            },
-            time: 1730,
-            date: 24052016,
-            view: true,
-            icon: "warninggeneral"
-        }
-    ];
+    $scope.events = [];
 
-    //fina the event with maximum level
+    //finalize the event with maximum level
     function getMaxLevel(events) {
         var maxLevel = 0;
         for (i = 0; i < events.length; i++) {
@@ -80,9 +31,6 @@ MAIN_MODULE.controller('eventCtrl', function($scope, $meteor, $reactive, $rootSc
     }
 
     // When the event array is not empty, the warning window will pop up
-    if ($scope.events.length != 0) {
-        popUpMulti.style.display = "block";
-    }
 
     //Close the pop-up windows when click on the x
     $scope.close = function () {
@@ -92,7 +40,18 @@ MAIN_MODULE.controller('eventCtrl', function($scope, $meteor, $reactive, $rootSc
     var reload = function () {
         $reactive(this).attach($scope);
         var events = $scope.getReactively('criticalEvents');
-        console.log($scope.criticalEvents);
+
+        if ($scope.events.length != 0) {
+            for(i = 0; i < events.length ; i++){
+                $scope.coord_lat = $scope.criticalEvents[i]['data']['coord_lat']
+                $scope.coord_lng = $scope.criticalEvents[i]['data']['coord_lon']
+            }
+            popUpMulti.style.display = "block";
+        }
+
+        $scope.events.push($scope.criticalEvents);
+        console.log($scope.events)
+
     };
     $scope.autorun(reload)
     
