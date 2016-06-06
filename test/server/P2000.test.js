27d3cab815be70652a2493c863db7d873a06dbbf
Martijn Copier <martijn_copier_4@hotmail.com>
2016-06-06 14:07:44 +0200

    Added new backend tests and removed winddirections

--- test/server/P2000.test.js
+++ test/server/P2000.test.js
@@ -1,12 +1,12 @@
 import { assert } from 'meteor/practicalmeteor:chai';
-
 //to be tested functions
-import {createP2000Data, pushP2000ToOrion, P2000Pull} from '/server/P2000.js';
+import {pushP2000ToOrion, P2000Pull} from '/server/P2000.js';
+import {pull} from './server/imports/orionAPI.js';
 
-
-describe('createP2000Data()', function(){
-	//no tests yet
-});
-describe('pushP2000ToOrion()', function(){
-	//no tests yet
+describe('P2000Pull() and pushP2000ToOrion()', function(){
+    it('Should not return error', function() {
+        expect(function(){
+            pushP2000ToOrion();
+        }).to.not.throw();
+    });
 });
---
+++ test/server/soundSensor.test.js
@@ -0,0 +1,12 @@
+import { assert } from 'meteor/practicalmeteor:chai';
+
+//to be tested functions
+import {SoundDataPull} from '/server/soundSensor.js';
+
+describe('gasSensorPull and smokeSensorPull and criticalEventPush()', function(){
+    it('Should not return error', function() {
+        expect(function(){
+            var test1 = SoundDataPull;
+        }).to.not.throw();
+    });
+});
