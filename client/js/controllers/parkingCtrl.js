3e0c336e5442c6b4f4fe4888b1b1bce916557b78
xndingo <xndingo@outlook.com>
2016-06-09 16:29:55 +0200

    working dynamic parking circle capacity percentage

--- client/js/controllers/parkingCtrl.js
+++ client/js/controllers/parkingCtrl.js
@@ -39,6 +39,24 @@ MAIN_MODULE.controller('parkingCtrl', function ($scope, $meteor, $reactive, $roo
         }
     };
 
+    /** Function that changes the freeColor when the percentage changes
+     * @var {string} changeColor
+     */
+    var changeColor = function() {
+        if ($scope.occupied <= $scope.capacity) {
+            if ($scope.occupied <= Math.round($scope.capacity / 2)) {
+                $scope.freeColor = 'green';
+            }
+            else if ($scope.occupied > Math.round($scope.capacity / 2) && $scope.occupied != $scope.capacity) {
+                $scope.freeColor = 'orange';
+            }
+            else {
+                $scope.freeColor = 'red';
+            }
+        }
+    }
+    $scope.autorun(changeColor);
+
     /** Defines the colors of percentage circle */
     $scope.color = {
         center : 'white',
@@ -50,30 +68,8 @@ MAIN_MODULE.controller('parkingCtrl', function ($scope, $meteor, $reactive, $roo
         if(error !== null) {
             $scope.capacity = result['spaces']['total'];
             $scope.occupied = result['occupied']['total'];
-        }//do stuff with result, handle error if not null
-    })
-
-    /** The percentage of the total amount of occupied spaces for all parking areas */
-    $scope.percent = Math.round($scope.occupied/$scope.capacity);
-
-    /** Function that changes the freeColor when the percentage changes
-     * @var {string} changeColor
-     */
-    var changeColor = function() {
-        console.log($scope.occupied);
-        if ($scope.occupied <= $scope.capacity) {
-            if ($scope.occupied <= Math.round($scope.capacity/2)) {
-                $scope.freeColor = 'green';
-            }
-            else if ($scope.occupied > Math.round(capacity/2) && $scope.occupied != $scope.capacity) {
-                $scope.freeColor = 'orange';
-            }
-            else {
-                $scope.freeColor = 'red';
-            }
         }
-    }
-    $scope.autorun(changeColor)
+    })
 
     /** TODO: Make sure this works with the db structure and gets outputed with the html when that's done
      * @summary Updates the scope information to the current selected parking area when a marker is clicked
@@ -92,10 +88,12 @@ MAIN_MODULE.controller('parkingCtrl', function ($scope, $meteor, $reactive, $roo
             var price = arg.price.split("_"); //splits the price entry to the hourly and daily prices
             $scope.pricehour = price[0]; //hourly fee
             $scope.priceday = price[1]; //daily fee
+
             Meteor.call('aggregateParking', function(error, result){
                 if(error !== null) {
                     $scope.capacity = result['spaces'][arg.index]; //get the amount of parking spaces for this parking area
                     $scope.occupied = result['occupied'][arg.index]; //get current occupancy number for this parking area
+                    $scope.percent = ($scope.occupied/$scope.capacity)*100;
                 }
             })
             $scope.$apply();
@@ -142,8 +140,6 @@ MAIN_MODULE.controller('parkingCtrl', function ($scope, $meteor, $reactive, $roo
                 $scope.markers[i].setMap(null);
             }
         }
-        //console.log(parkingAreas);
     }
     $scope.autorun(reload);
-    
 });
